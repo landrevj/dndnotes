1a6e932bb99fc319dd13e122ce291e6553c19d88
Joseph Landreville <landrevillejoseph@gmail.com>
2018-12-29 19:32:44 -0800

    Enforce link uniqueness.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,13 @@
 class Link < ApplicationRecord
+  validate :unique_link
+
   belongs_to :origin, polymorphic: true
   belongs_to :linkable, polymorphic: true
+
+  def unique_link
+    if Link.exists?(origin_id: origin.id, origin_type: origin.model_name.name, linkable_id: linkable.id, linkable_type: linkable.model_name.name) || 
+       Link.exists?(origin_id: linkable.id, origin_type: linkable.model_name.name, linkable_id: origin.id, linkable_type: origin.model_name.name)
+      errors.add(:Link, "already exists")
+    end
+  end
 end
