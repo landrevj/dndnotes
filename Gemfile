824334ab0823a10ab4f268623b00bd5121672d94
Joseph Landreville <landrevillejoseph@gmail.com>
2018-12-30 18:55:36 -0800

    Install Devise.

--- Gemfile
+++ Gemfile
@@ -27,6 +27,12 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+#################################
+# Use Devise for user authentication
+gem 'devise'
+
+#################################
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  devise_for :users
+
   resources :quests
   resources :locations
   resources :campaigns
-  root 'static_pages#home'
   resources :links, only: [:new, :create, :destroy]
   resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
