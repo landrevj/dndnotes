69c608022433a3b7e325a8c44ea48873acb7ec4c
Joseph Landreville <landrevillejoseph@gmail.com>
2018-12-30 20:41:46 -0800

    Enforce authentication except on landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  
+  # different roots for authenticated users
+  authenticated :user do
+    root to: 'static_pages#user_home', as: :authenticated_root
+  end
   root 'static_pages#home'
+
   devise_for :users, controllers: { registrations: "registrations" }
 
+  # resources
   resources :quests
   resources :locations
   resources :campaigns
   resources :links, only: [:new, :create, :destroy]
   resources :notes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
