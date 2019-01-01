85e5a130d64eb038643e7010126c0781db2f3837
Joseph Landreville <landrevillejoseph@gmail.com>
2019-01-01 00:43:18 -0800

    Style Rails toasts with bootstrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,14 @@ module ApplicationHelper
 
     markdown.render(text).html_safe
   end
+
+  def flash_class(key)
+    case key
+      when 'notice'  then 'alert alert-info'
+      when 'success' then 'alert alert-success'
+      when 'error'   then 'alert alert-error'
+      when 'alert'   then 'alert alert-error'
+    end
+  end
+
 end
