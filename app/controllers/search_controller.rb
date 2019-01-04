1f60ba663da6ac18bcb08a4a50e4d13bba01f95a
Joseph Landreville <landrevillejoseph@gmail.com>
2019-01-04 01:20:51 -0800

    Limit search controller results to 10.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,10 @@ class SearchController < ApplicationController
         type = params[:type]
         if type == 'notes'
             @name = false 
-            @objects = current_user.send(type).ransack(content_cont: params[:q]).result(distinct: true)
+            @objects = current_user.send(type).ransack(content_cont: params[:q]).result(distinct: true).limit(10)
         else
             @name = true
-            @objects = current_user.send(type).ransack(name_cont: params[:q]).result(distinct: true)
+            @objects = current_user.send(type).ransack(name_cont: params[:q]).result(distinct: true).limit(10)
         end
             
     end
