d6bc4b3677ca7df646c73791201b1eb433344f7d
Joseph Landreville <landrevillejoseph@gmail.com>
2019-04-01 17:29:28 -0700

    Remove category name uniqueness validation.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :notes, dependent: :destroy
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
 end
