b55b814b3667b20c3d529672004d71c621dcf49d
Joseph Landreville <landrevillejoseph@gmail.com>
2019-04-04 17:59:40 -0700

    Move color list into category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,18 @@ class Category < ApplicationRecord
     has_many :notes, dependent: :destroy
 
     validates :name, presence: true
+
+    COLORS = [
+        ['Red', 'red'],
+        ['Pink', 'pink'],
+        ['Orange', 'orange'],
+        ['Yellow', 'yellow'],
+        ['Green', 'green'],
+        ['Teal', 'teal'],
+        ['Blue', 'blue'],
+        ['Cyan', 'cyan'],
+        ['Purple', 'purple'],
+        ['Indigo', 'indigo'],
+        ['Gray', 'gray'],
+        ];
 end
