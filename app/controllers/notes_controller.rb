dd9d66a9dafe2832079afae054df6e1065ad37c6
Joey Landreville <16bitbow@gmail.com>
2019-04-27 18:22:58 -0700

    Allow users to change Category of Note.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -22,6 +22,7 @@ class NotesController < ApplicationController
 
   # GET /notes/1/edit
   def edit
+    console
     @linked_notes = @note.linked_notes.group_by(&:category).sort_by { |c, _| c.name }
   end
 
