2ed70386d925a1c047ef1634fd9f32ab3693f856
Joey Landreville <16bitbow@gmail.com>
2019-05-05 00:28:12 -0700

    Add searchbar for all notes in workspace to navbar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,18 @@
 class SearchController < ApplicationController
-    def search
-        @category = Category.find(params[:category_id])
-        @notes = @category.notes.ransack(name_cont: params[:q]).result(distinct: true).limit(10)
+  def search
+    respond_to do |format|
+      format.html {
+        @q = current_user.active_workspace.notes.ransack(params[:q])
+        @notes = @q.result(distinct: true).limit(10)
+      }
+      format.json {
+        if params.key?(:category_id)
+          @category = Category.find(params[:category_id])
+          @notes = @category.notes.ransack(name_cont: params[:q]).result(distinct: true).limit(10)
+        else
+          @notes = current_user.active_workspace.notes.ransack(name_cont: params[:q]).result(distinct: true).limit(10)
+        end
+      }
     end
+  end
 end
--- app/views/search/search.json.jbuilder
+++ app/views/search/search.json.jbuilder
@@ -1,6 +1,8 @@
-json.motif_color @category.color
-json.category_id @category.id
+if @category
+  json.motif_color @category.color
+  json.category_id @category.id
+end
 json.notes @notes do |note|
-    json.text note.name
-    json.id   note.id
+  json.text note.name
+  json.id   note.id
 end
